241d9f9674acb663cc347b4f5fa82da1e88bb76e
davidyan7 <davidyan7@gmail.com>
2023-05-28 17:39:48 +0300

    Updates

---
+++ precache-manifest.bb2a5cb904147f89d6c04f4d10b84e05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b65e1da47996400725018e30389a25",
+    "url": "/mada_app/index.html"
+  },
+  {
+    "revision": "887ec18a5a491b58e73a",
+    "url": "/mada_app/static/css/2.84a2c760.chunk.css"
+  },
+  {
+    "revision": "e69a5ff96635869b836e",
+    "url": "/mada_app/static/css/main.e1bbf1a5.chunk.css"
+  },
+  {
+    "revision": "887ec18a5a491b58e73a",
+    "url": "/mada_app/static/js/2.531a1a9e.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/mada_app/static/js/2.531a1a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69a5ff96635869b836e",
+    "url": "/mada_app/static/js/main.eb3ff61a.chunk.js"
+  },
+  {
+    "revision": "651e671b677361dbc4fe",
+    "url": "/mada_app/static/js/runtime-main.a1d00546.js"
+  },
+  {
+    "revision": "b1ec5d58c9e5504727e51846a7a054d9",
+    "url": "/mada_app/static/media/sound.b1ec5d58.mp3"
+  }
+]);
